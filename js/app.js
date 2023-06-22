22f833555218ea4308a35997cb2c99e41df10c7d
Roaa Abu-shaqrah <abushaqrah.roaa@gmail.com>
2023-06-22 23:49:59 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
+/* get all section tags*/
 let sections =document.getElementsByTagName("section");
 
 
  for(let i=0; i<sections.length;i++){
   let List =document.getElementById('navbar__list')
-  let li =document.createElement('li');
-  let link = document.createElement('a');
-  let att = document.createAttribute("data-id");
+  let li =document.createElement('li');/*create li element*/
+  let link = document.createElement('a');/*create a element*/
+  let att = document.createAttribute("data-id"); /*create attribute element*/
   let sectionId = sections[i].getAttribute('id');
   att.value = sectionId; 
   link.setAttributeNode(att)
   link.innerHTML=sectionId
-  li.appendChild(link)
+  li.appendChild(link) /* append <li><a></li></a>*/
   // li.innerHTML =sectionId;
 
 List.append(li)
@@ -19,26 +20,7 @@ List.append(li)
  }
 
 
- function isInViewport (elem) {
-  const bounding = elem.getBoundingClientRect();
-  return (
-      bounding.top >= 0 &&
-      bounding.left >= 0 &&
-      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
 
-
-function setActiveClass(){
-  for (let i=0; i < sections.length; i++){
-      if (isInViewport(sections[i])){
-          sections[i].classList.add("your-active-class");
-      }else{
-          section[i].classList.remove("your-active-class");
-      }
-  }
-}
 
 function scrollToElement(event){
   if(event.target.nodeName === 'A'){
@@ -57,37 +39,46 @@ navBarList.addEventListener('click', function(event){
 
 
 
-const links = document.querySelectorAll('section');
-  function changeLinkState() {
-    let index = links.length;
+/*When the section is clicked in navbar, it should be highlighted and should stay highlighted when user clicks outside on webpage*/
+let nav=document.getElementById('navbar__list')/*get ul */
+let list=document.getElementsByTagName('li')/*get all li tag*/
+for (let i = 0; i < list.length; i++) {
+  /*for loop li tag**/
+  list[i].addEventListener("click", function() {
+  var active = document.getElementsByClassName("active");
+  if (active.length > 0) { 
+    /*check if length > 0 so it active other wise ""*/
+    active[0].className = active[0].className.replace(" active", ""); 
+  }
+  this.className += " active";
+  });
+}
 
-    while(--index && window.scrollY + 50 < links[index].offsetTop) {}
+/* scroll the page the section on navebaer active*/
+document.addEventListener("DOMContentLoaded", function() {
+  const navLinks = document.querySelectorAll("nav ul li a");/*get all elemnts*/
+  const sections = document.querySelectorAll("section");
 
-    links.forEach((link) => link.classList.remove('active'));
-    links[index].classList.add('active');
+  function setActiveLink() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+    sections.forEach(function(section, index) {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+        navLinks.forEach(function(link) {
+                link.classList.remove("active");/*remove class active when scroll another*/
+        });
+        navLinks[index].classList.add("active");/*add class active when scroll another*/
+      }
+    });
   }
 
-changeLinkState();
-window.addEventListener('scroll', changeLinkState);
-
-
-const mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
+  window.addEventListener("scroll", setActiveLink); /*active section*/
+});
+function removeActiveClass(navLinks) {
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].classList.remove('active');
+    }
   }
-}
-//
-
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
